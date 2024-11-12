82c670b89017101bcd70f49e45406358bfd6b245
Victor <147107766+Crosby01@users.noreply.github.com>
2024-11-12 16:10:22 +0100

    Add files via upload

---
+++ weightConversion/src/main/java/com/mycompany/weightconversion/WeightConversion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.weightconversion;
+
+/**
+ *
+ * @author Victor
+ */
+public class WeightConversion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
